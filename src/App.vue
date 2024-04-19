6d422a4be01546a5919c3d0341e01e865c23ba1a
Sailwayfun <liaoleon000513@gmail.com>
2024-04-19 20:49:10 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-red-500 mx-auto text-2xl">Hello Tailwind</h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="postcss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply mt-16 text-center;
 }
 </style>
